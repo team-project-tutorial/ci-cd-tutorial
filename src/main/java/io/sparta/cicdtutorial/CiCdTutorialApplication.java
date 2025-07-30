b255756beff4a52670bcd3706bba5852a23b5369
choi-ys <project.log.062@gmail.com>
2025-07-30 11:30:16 +0900

    initial commit

---
+++ src/main/java/io/sparta/cicdtutorial/CiCdTutorialApplication.java
@@ -0,0 +1,13 @@
+package io.sparta.cicdtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/sparta/cicdtutorial/CiCdTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sparta.cicdtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
