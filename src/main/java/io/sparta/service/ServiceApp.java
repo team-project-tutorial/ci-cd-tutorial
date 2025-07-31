4176268045dd04aca3419847c33e13294baa866f
choi-ys <project.log.062@gmail.com>
2025-08-01 08:28:19 +0900

    config: add spring boot application entry point

--- src/main/java/io/sparta/service/ServiceApp.java
+++ src/main/java/io/sparta/service/ServiceApp.java
@@ -1,13 +1,13 @@
-package io.sparta.cicdtutorial;
+package io.sparta.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CiCdTutorialApplication {
+public class ServiceApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CiCdTutorialApplication.class, args);
+		SpringApplication.run(ServiceApp.class, args);
 	}
 
 }
